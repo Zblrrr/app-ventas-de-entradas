768c12db8874015283bb50b786ac2bc13271f22a
Zblrrr <felipe.crisostomo100@gmail.com>
2024-09-11 17:03:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App Ventas de Entradas',
+  webDir: 'www'
+};
+
+export default config;
